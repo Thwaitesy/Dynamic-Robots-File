fdce1d4852af73efdfffb0e124ae42aa95bd397a
Sam Thwaites <sam@thwaitesit.com>
2012-10-13 18:58:46 +1000

    Initial checkin

---
+++ MvcApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Robots.txt",
+                url: "Robots.txt",
+                defaults: new { controller = "Robots", action = "RobotsTextFile" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/RobotsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class RobotsController : Controller
+    {
+        public FileContentResult RobotsTextFile()
+        {
+            var content = "User-agent: *" + Environment.NewLine;
+
+            if (string.Equals(ConfigurationManager.AppSettings["allow-robots"], "true", StringComparison.InvariantCultureIgnoreCase))
+            {
+                content += "Disallow: /Admin" + Environment.NewLine;
+                content += "Sitemap: http://www.your-website-here.com/sitemap.xml" + Environment.NewLine;
+            }
+            else
+            {
+                content += "Disallow: /" + Environment.NewLine;
+            }
+
+            return File(Encoding.UTF8.GetBytes(content), "text/plain");
+        }
+    }
+}
